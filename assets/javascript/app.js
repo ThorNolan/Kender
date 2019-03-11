f25c3cad6dadee1f4a349976b62be3e44ea970cd
atiftariq786 <atiftariq786@hotmail.com>
2019-03-11 10:52:34 -0700

    Modified index.html and app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,14 @@
 
 // TESTED API 
 
-var inputLocation = "San Francisco";
 
+//$("#userSubmit").on("click", function() {
+
+    
+
+
+    var inputLocation = "london";//$("#userInput").val();
+    var unit = "&units=imperial";
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ inputLocation +"&APPID=f7d032505cb605fdfe25eebe96d9ab15";
  
@@ -13,8 +19,14 @@ var inputLocation = "San Francisco";
    }).then(function (response) {
         
        console.log(response);
+
+       
  
    })
+
+//})
+
+
 
 // We can get weather[0"] , rain[1h"] ,name , temperature wind speed and more from api response
 
