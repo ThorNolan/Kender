7b4be1f224cfe0d260a3924545ffa9cbfb488d70
Sam Thompson <electronic62794@yahoo.com>
2019-03-13 11:42:58 -0700

    SPOTIFY WORKSgit add .git add .

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,32 +6,32 @@
 
 //$("#userSubmit").on("click", function() {
 
-    
 
 
-    var inputLocation = "london";//$("#userInput").val();
-    var unit = "&units=imperial";
+
+var inputLocation = "london";//$("#userInput").val();
+var unit = "&units=imperial";
 
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputLocation + "&APPID=f7d032505cb605fdfe25eebe96d9ab15";
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
+  url: queryURL,
+  method: "GET"
 }).then(function (response) {
 
-    console.log(response);
+  console.log(response);
 
 })
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputLocation + "&APPID=f7d032505cb605fdfe25eebe96d9ab15";
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
+  url: queryURL,
+  method: "GET"
 }).then(function (response) {
 
-    console.log(response);
+  console.log(response);
 
 })
 
@@ -58,7 +58,7 @@ $.ajax({
 //-Atif-------Weather Search Detail Firebase Database---------------
 
 // Initialize Firebase
-        
+
 /*var config = {
   apiKey: "AIzaSyC7sFmSCyeTZUQnW-wof8SBv4EV5uLvsxA",
   authDomain: "weather-project-d144f.firebaseapp.com",
@@ -80,87 +80,87 @@ var database = firebase.database();
 //-Atif-------Weather Search By click on Submit---------------
 var mood;
 $(".row").hide();
-$("#find-weather").on("click", function(event){
+$("#find-weather").on("click", function (event) {
 
-    event.preventDefault();
-    
-    $(".row").show();
+  event.preventDefault();
 
-    var inputLocation = $("#weatherInput").val();     
-    
-   
+  $(".row").show();
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ inputLocation +"&units=imperial&APPID=f7d032505cb605fdfe25eebe96d9ab15&cnt=3";
- 
-     $.ajax({
-       url: queryURL,
-       method: "GET"
-   }).then(function (response) {
-        
-       
-       var apiResponse = response;
-      var responseList = response.list;
-      var todayRainStatus = responseList[0].weather[0].description;
-       console.log(apiResponse);
-      console.log(responseList);
-
-       for(var i=0; i<responseList.length;i++){
-
-        
-        var cardNum = i+1;
-
-        $("#card-"+cardNum).empty();
-      
-       var cityName = apiResponse.city.name;
-
-       $("#card-"+cardNum).append("City Name:  "+ cityName);
+  var inputLocation = $("#weatherInput").val();
 
 
-       var country = apiResponse.city.country;
-       $("#card-"+cardNum).append("<p> Country Name  :" + country + " </p>");
 
-       var windSpeed = responseList[i].wind.speed;
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputLocation + "&units=imperial&APPID=f7d032505cb605fdfe25eebe96d9ab15&cnt=3";
 
-       $("#card-"+cardNum).append("<p> Wind Speed  :" + windSpeed + " </p>");
-       
-       var pressure = responseList[i].main.pressure;
-       $("#card-"+cardNum).append("<p> Pressure  :" + pressure + " </p>");
-
-       var temperature = responseList[i].main.temp;
-       $("#card-"+cardNum).append("<p>  Temperature (F)  :" + temperature + " </p>");
-        
-       var rainStatus = responseList[i].weather[0].description;
-       $("#card-"+cardNum).append("<p> Rain Status  :" + rainStatus + " </p>");
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
 
-            
+    var apiResponse = response;
+    var responseList = response.list;
+    var todayRainStatus = responseList[0].weather[0].description;
+    console.log(apiResponse);
+    console.log(responseList);
 
-           
-            
-       }
-       
-       
-       // using information we gathered from the weather API, use that to assign a search term "mood" to a spotify playlist search
-       if (todayRainStatus === "clear sky") {
-         mood = "happy";
-       }
-       
-       if (todayRainStatus.includes("clouds")) {
-         mood = "chill";
-       }
-       if (todayRainStatus.includes("rain")) {
-         mood = "sad";
-       }
-       if (todayRainStatus.includes("thunderstsorm")) {
-         mood = "angry";
-       }
-       if (todayRainStatus.includes( "snow")) {
-         mood = "lo-fi";
-       }
-       
+    for (var i = 0; i < responseList.length; i++) {
 
-       spotifySearch();
-   })
+
+      var cardNum = i + 1;
+
+      $("#card-" + cardNum).empty();
+
+      var cityName = apiResponse.city.name;
+
+      $("#card-" + cardNum).append("City Name:  " + cityName);
+
+
+      var country = apiResponse.city.country;
+      $("#card-" + cardNum).append("<p> Country Name  :" + country + " </p>");
+
+      var windSpeed = responseList[i].wind.speed;
+
+      $("#card-" + cardNum).append("<p> Wind Speed  :" + windSpeed + " </p>");
+
+      var pressure = responseList[i].main.pressure;
+      $("#card-" + cardNum).append("<p> Pressure  :" + pressure + " </p>");
+
+      var temperature = responseList[i].main.temp;
+      $("#card-" + cardNum).append("<p>  Temperature (F)  :" + temperature + " </p>");
+
+      var rainStatus = responseList[i].weather[0].description;
+      $("#card-" + cardNum).append("<p> Rain Status  :" + rainStatus + " </p>");
+
+
+
+
+
+
+    }
+
+
+    // using information we gathered from the weather API, use that to assign a search term "mood" to a spotify playlist search
+    if (todayRainStatus === "clear sky") {
+      mood = "happy";
+    }
+
+    if (todayRainStatus.includes("clouds")) {
+      mood = "chill";
+    }
+    if (todayRainStatus.includes("rain")) {
+      mood = "sad";
+    }
+    if (todayRainStatus.includes("thunderstsorm")) {
+      mood = "angry";
+    }
+    if (todayRainStatus.includes("snow")) {
+      mood = "lo-fi";
+    }
+
+
+    spotifySearch();
+  })
 
 });
 
@@ -182,63 +182,63 @@ $("#find-weather").on("click", function(event){
 // Eventbrite pull and ajax call
 $(document).ready(function () {
 
-    // this array is for the href of the carousel items, which is necessary as it's how materialize keeps track of each slide
-    var numbersArr = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen"];
+  // this array is for the href of the carousel items, which is necessary as it's how materialize keeps track of each slide
+  var numbersArr = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen"];
 
-    // fix for potential cors error
-    jQuery.ajaxPrefilter(function (options) {
-        if (options.crossDomain && jQuery.support.cors) {
-            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-        }
-    });
+  // fix for potential cors error
+  jQuery.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+  });
 
-    $(document).on("submit", ".event-form", function (event) {
-        event.preventDefault()
+  $(document).on("submit", ".event-form", function (event) {
+    event.preventDefault()
 
-        var paid = $("#paid").val()
-        var eventName = $("#eventName").val()
-        var location = $("#location").val()
+    var paid = $("#paid").val()
+    var eventName = $("#eventName").val()
+    var location = $("#location").val()
 
-        $.ajax({
-            url: 'https://www.eventbriteapi.com/v3/events/search/?q=' + eventName + '&price=' + paid + '&location.address=' + location + '&token=RQIFLDPFZLH3JYH4WYJQ',
-            method: "GET"
-        }).then(function (response) {
-            console.log(response)
-            // response.events.length
-            for (var i = 0; i < 15; i++) {
-                var newSlide = makeEventCarousel(response.events[i], i);
-                $(".appendEventsHere").append(newSlide);
-            }
-            if ($(".carousel").hasClass("initialized")) {
-                $(".carousel").removeClass("initialized");
-            } else {
-                $('.carousel.carousel-slider').carousel({
-                    fullWidth: true,
-                    indicators: true
-                });
-            }
+    $.ajax({
+      url: 'https://www.eventbriteapi.com/v3/events/search/?q=' + eventName + '&price=' + paid + '&location.address=' + location + '&token=RQIFLDPFZLH3JYH4WYJQ',
+      method: "GET"
+    }).then(function (response) {
+      console.log(response)
+      // response.events.length
+      for (var i = 0; i < 15; i++) {
+        var newSlide = makeEventCarousel(response.events[i], i);
+        $(".appendEventsHere").append(newSlide);
+      }
+      if ($(".carousel").hasClass("initialized")) {
+        $(".carousel").removeClass("initialized");
+      } else {
+        $('.carousel.carousel-slider').carousel({
+          fullWidth: true,
+          indicators: true
+        });
+      }
 
-            $('.modal').modal();
+      $('.modal').modal();
 
-            // function for building carousel pieces 
-            function makeEventCarousel(eventInfo, num) {
+      // function for building carousel pieces 
+      function makeEventCarousel(eventInfo, num) {
 
-                //Build carousel pieces
-                var newItem = $("<div>").addClass("carousel-item").attr("href", "#" + numbersArr[num] + "!");
-                var eventImage = $("<img>").attr("src", eventInfo.logo.original.url);
-                var eventTitle = $("<h2>").attr("text", eventInfo.name.text)
-                // var eventDescription = $("<p>").attr("text", eventInfo.description.text);
+        //Build carousel pieces
+        var newItem = $("<div>").addClass("carousel-item").attr("href", "#" + numbersArr[num] + "!");
+        var eventImage = $("<img>").attr("src", eventInfo.logo.original.url);
+        var eventTitle = $("<h2>").attr("text", eventInfo.name.text)
+        // var eventDescription = $("<p>").attr("text", eventInfo.description.text);
 
-                // build modal trigger buttons for each carousel item
-                var newModalBtnHolder = $("<div>").addClass("carousel-fixed-item center");
-                var newModalBtn = $("<a>").addClass("waves-effect waves-light btn modal-trigger").attr("href", "#modal" + num).text("More Info");
+        // build modal trigger buttons for each carousel item
+        var newModalBtnHolder = $("<div>").addClass("carousel-fixed-item center");
+        var newModalBtn = $("<a>").addClass("waves-effect waves-light btn modal-trigger").attr("href", "#modal" + num).text("More Info");
 
-                // build modal popups, triggered by the buttons above
-                var newModalHolder = $("<div>").addClass("modal").attr("id", "modal" + num);
-                var modalDiv = $("<div>").addClass("modal-content");
-                var modalDescription = $("<p>").text(eventInfo.description.text);
-                var modalFooterDiv = $("<div>").addClass("modal-footer");
-                var modalFooterItem = $("<a>").addClass("modal-close waves-effect waves-green btn-flat").text("Close");
+        // build modal popups, triggered by the buttons above
+        var newModalHolder = $("<div>").addClass("modal").attr("id", "modal" + num);
+        var modalDiv = $("<div>").addClass("modal-content");
+        var modalDescription = $("<p>").text(eventInfo.description.text);
+        var modalFooterDiv = $("<div>").addClass("modal-footer");
+        var modalFooterItem = $("<a>").addClass("modal-close waves-effect waves-green btn-flat").text("Close");
 
         // <div id="modal1" class="modal">
         // <div class="modal-content">
@@ -249,26 +249,26 @@ $(document).ready(function () {
         // <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
         // </div>
         // </div>
-            
 
-                // append my carousel pieces
-                newItem.append(eventImage);
-                // newItem.append(eventDescription);
-                newItem.append(eventTitle);
-                newModalBtnHolder.append(newModalBtn);
-                newItem.append(newModalBtnHolder);
 
-                // append my modal button to each carousel item div
-                modalDiv.append(modalDescription);
-                modalFooterDiv.append(modalFooterItem);
-                newModalHolder.append(modalDiv, modalFooterDiv);
-                $(".modalHolder").append(newModalHolder);
+        // append my carousel pieces
+        newItem.append(eventImage);
+        // newItem.append(eventDescription);
+        newItem.append(eventTitle);
+        newModalBtnHolder.append(newModalBtn);
+        newItem.append(newModalBtnHolder);
 
-                return newItem;
-            }
+        // append my modal button to each carousel item div
+        modalDiv.append(modalDescription);
+        modalFooterDiv.append(modalFooterItem);
+        newModalHolder.append(modalDiv, modalFooterDiv);
+        $(".modalHolder").append(newModalHolder);
 
-        });
-    })
+        return newItem;
+      }
+
+    });
+  })
 
 })
 
@@ -281,70 +281,84 @@ $(document).ready(function () {
 
 
 function spotifySearch() {
-  var token = "BQDjYbdd3lG2b6qGNjwuWIOI4WXltvrh-yV-2-pQoQZKYRYGz-MQkUVYhpPLSvHu5UWLyWDSvY_uriZ-USe09zwoWpLDzGvUJDyn4vzryLrPUpmuqYQDrvzhvfIv8Bwn0w07KT52ZytV6h28oWuabJSce76BjRpwklX4pvutKn2J_V9ZeY_1evU59vTuLhg2u5xvQW7AK9eLcLlkDjFYy03BLBJ73LeFtISBZdYVI9JGNiEN6Y1XOdRnZVGK6VePhqeKSs1ENR6Dh9OwSSM"
-  
+  var token = "BQDgGRWgZLRxGLJ7YhevF-QcITwjH6U4nB2R8n5w7nuLR7fBkjqQ01V399eKZCBMCi5xJpGO_azLekFJhNE7g0hO9ss6C8zB-87MmzvrtEU6qUdjrzqQ8P65KfYaNAJtAdisWDnXYrsXOXhQhSC1goqaPAqFfs6T94zj2Po3QCVVYmUT0Z5g6G3fTyG-iZUzaX1fxImx-Hhg0yOlIWRMC6URJlSK1OV5gmEBj3BtOtQ5UP-GNcdER8huTU8nE0-jYmtHnZmR9TwjQ0MGVcs"
+
   var type = "playlist";
-  
+  var accessToken = "BQAoA0tEEWMoSpiBKOJ4YTDkIp8vXx48j1MRfOHqaKzqsmCi_QwLjG2EbQmw0VAlRpGbHJ8RI2OpLNaSKbxiio3RqpbQEjxvs-Y3pM0kCQbhuoi8K2VyRH5F4FFiP4rCqdZhbr_qPWGSzqgPtsLYoi0vcdj79KGCCuT3lZk9EYZpddQ07-3mAguF_IUx3c0"
+  var refreshToken = "AQBPGEZ_-JZI17n-Khd0DAf7l1_KIxgU3NpISIJf9fLNKshcspuHU-U85UCv_-sdVw5ZBkEPDHizgxyFv0GMpMUz7uVfosb3B6pBvqjcT-ywXQuK4WUnlfPsxZYj7zVmYfs37Q"
 
   $.ajax({
     url: 'https://api.spotify.com/v1/search?type=' + type + '&query=' + mood,
     headers: {
-      Authorization: 'Bearer ' + token
+      "access_token": accessToken,
+   "token_type": "Bearer",
+   "scope": "user-read-private user-read-email",
+   "expires_in": 3600,
+   "refresh_token": refreshToken
     }
   }).then(function (response) {
     function randomPlaylistInfo() {
 
       //random number between 0 and 19 to target a random playlist
       var i = randomNumber(20)
-      
+
       //variables to point to the Image, Link to playlist, and Playlist Name
       var playlists = response.playlists.items;
       var playlistImage = playlists[i].images[0].url;
       var playlistLinks = playlists[i].external_urls.spotify;
       var playlistName = playlists[i].name;
-    
-    
+      var playlistID = playlists[i].id
+
+
       //print the information
       console.log("Name: " + playlistName)
       console.log("Image: " + playlistImage)
       console.log("Playlist Link: " + playlistLinks)
-    
-    // displaying information onto a card
+
+      // displaying information onto a card
       $("#playlist-name").text("Playlist Name: " + playlistName);
       $("#playlist-link").attr("href", playlistLinks);
       $("#playlist-image").attr("src", playlistImage);
-    
+      $("#spotify-player").attr("src", "https://open.spotify.com/embed/playlist/" + playlistID);
+
     }
-      console.log(response);
-      randomPlaylistInfo();
-    })
-  }
+    console.log(response);
+    randomPlaylistInfo();
+  })
+}
 
-// using information we gathered from the weathe API, use that to assign a search term "mood" to a spotify playlist search
-// if (todayRainStatus === "clear sky") {
-//   mood = "happy";
-// }
 
-// if (todayRainStatus.includes("clouds")) {
-//   mood = "chill";
-// }
-// if (todayRainStatus.includes("rain")) {
-//   mood = "sad";
-// }
-// if (todayRainStatus.includes("thunderstsorm")) {
-//   mood = "angry";
-// }
-// if (todayRainStatus.includes( "snow")) {
-//   mood = "lo-fi";
-// }
-
-//global variables to make Ajax work
 
 
 //Random Number Generator function 
 function randomNumber(int) {
-    return Math.floor(Math.random() * int);
+  return Math.floor(Math.random() * int);
 }
+
+$("#happy").on("click", function () {
+  mood = "happy"
+  spotifySearch();
+})
+
+$("#sad").on("click", function () {
+  mood = "sad"
+  spotifySearch();
+})
+
+$("#chill").on("click", function () {
+  mood = "chill"
+  spotifySearch();
+})
+
+$("#angry").on("click", function () {
+  mood = "angry"
+  spotifySearch();
+})
+
+$("#lo-fi").on("click", function () {
+  mood = "lo-fi"
+  spotifySearch();
+})
 
 
 
